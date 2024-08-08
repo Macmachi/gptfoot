ea4174d8e8ffd2a437ea7a9d46e2cea8b4226c1e
Rymentz <38922703+Macmachi@users.noreply.github.com>
2024-08-08 10:44:49 +0200

    v2.2.6

--- gptfoot.py
+++ gptfoot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # AUTEUR :  Arnaud R. (https://github.com/Macmachi/gptfoot) 
-# VERSION : v2.2.5
+# VERSION : v2.2.6
 # LICENCE : Attribution-NonCommercial 4.0 International
 #
 import asyncio
